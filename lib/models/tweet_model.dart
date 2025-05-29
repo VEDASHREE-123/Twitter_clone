495a4c5d10be4a961edc3b5ac0170534c3e0d63f
VEDASHREE-123 <142390559+VEDASHREE-123@users.noreply.github.com>
2025-05-29 22:22:45 +0530

    Add files via upload

---
+++ lib/models/tweet_model.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:twitter_clone/core/enums/tweet_type_enum.dart';
+
+@immutable
+class Tweet {
+  final String id;
+  final String text;
+  final List<String> hashTags;
+  final String link;
+  final List<String> imageLinks;
+  final String uid;
+  final TweetType tweetType;
+  final DateTime tweetedAt;
+  final List<String> likes;
+  final List<String> commentIds;
+  final int reShareCount;
+  final String reTweetedBy;
+  final String repliedTo;
+
+  const Tweet({
+    required this.id,
+    required this.text,
+    required this.hashTags,
+    required this.link,
+    required this.imageLinks,
+    required this.uid,
+    required this.tweetType,
+    required this.tweetedAt,
+    required this.likes,
+    required this.commentIds,
+    required this.reShareCount,
+    required this.reTweetedBy,
+    required this.repliedTo,
+  });
+
+  Tweet copyWith({
+    String? id,
+    String? text,
+    List<String>? hashTags,
+    String? link,
+    List<String>? imageLinks,
+    String? uid,
+    TweetType? tweetType,
+    DateTime? tweetedAt,
+    List<String>? likes,
+    List<String>? commentIds,
+    int? reShareCount,
+    String? reTweetedBy,
+    String? repliedTo,
+  }) {
+    return Tweet(
+      id: id ?? this.id,
+      text: text ?? this.text,
+      hashTags: hashTags ?? this.hashTags,
+      link: link ?? this.link,
+      imageLinks: imageLinks ?? this.imageLinks,
+      uid: uid ?? this.uid,
+      tweetType: tweetType ?? this.tweetType,
+      tweetedAt: tweetedAt ?? this.tweetedAt,
+      likes: likes ?? this.likes,
+      commentIds: commentIds ?? this.commentIds,
+      reShareCount: reShareCount ?? this.reShareCount,
+      reTweetedBy: reTweetedBy ?? this.reTweetedBy,
+      repliedTo: repliedTo ?? this.repliedTo,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'text': text});
+    result.addAll({'hashTags': hashTags});
+    result.addAll({'link': link});
+    result.addAll({'imageLinks': imageLinks});
+    result.addAll({'uid': uid});
+    result.addAll({'tweetType': tweetType.type});
+    result.addAll({'tweetedAt': tweetedAt.millisecondsSinceEpoch});
+    result.addAll({'likes': likes});
+    result.addAll({'commentIds': commentIds});
+    result.addAll({'reShareCount': reShareCount});
+    result.addAll({'reTweetedBy': reTweetedBy});
+    result.addAll({'repliedTo': repliedTo});
+
+    return result;
+  }
+
+  factory Tweet.fromMap(Map<String, dynamic> map) {
+    return Tweet(
+      id: map['\$id'] ?? '',
+      text: map['text'] ?? '',
+      hashTags: List<String>.from(map['hashTags']),
+      link: map['link'] ?? '',
+      imageLinks: List<String>.from(map['imageLinks']),
+      uid: map['uid'] ?? '',
+      tweetType: (map['tweetType'] as String).toTweetTypeEnum(),
+      tweetedAt: DateTime.fromMillisecondsSinceEpoch(map['tweetedAt']),
+      likes: List<String>.from(map['likes']),
+      commentIds: List<String>.from(map['commentIds']),
+      reShareCount: map['reShareCount']?.toInt() ?? 0,
+      reTweetedBy: map['reTweetedBy'] ?? '',
+      repliedTo: map['repliedTo'] ?? '',
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Tweet(id: $id, text: $text, hashTags: $hashTags, link: $link, imageLinks: $imageLinks, uid: $uid, tweetType: $tweetType, tweetedAt: $tweetedAt, likes: $likes, commentIds: $commentIds, reShareCount: $reShareCount, reTweetedBy: $reTweetedBy, repliedTo: $repliedTo,)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Tweet &&
+        other.id == id &&
+        other.text == text &&
+        listEquals(other.hashTags, hashTags) &&
+        other.link == link &&
+        listEquals(other.imageLinks, imageLinks) &&
+        other.uid == uid &&
+        other.tweetType == tweetType &&
+        other.tweetedAt == tweetedAt &&
+        listEquals(other.likes, likes) &&
+        listEquals(other.commentIds, commentIds) &&
+        other.reShareCount == reShareCount &&
+        other.reTweetedBy == reTweetedBy &&
+        other.repliedTo == repliedTo;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        text.hashCode ^
+        hashTags.hashCode ^
+        link.hashCode ^
+        imageLinks.hashCode ^
+        uid.hashCode ^
+        tweetType.hashCode ^
+        tweetedAt.hashCode ^
+        likes.hashCode ^
+        commentIds.hashCode ^
+        reShareCount.hashCode ^
+        reTweetedBy.hashCode ^
+        repliedTo.hashCode;
+  }
+}
